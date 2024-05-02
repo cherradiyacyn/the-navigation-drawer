2b86914e237ebf2f83f80fd602957c05c09b02fe
Yacyn CHERRADI <cherradiyacyn@gmail.com>
2024-05-02 14:15:01 +0100

    init

---
+++ script.js
@@ -0,0 +1,28 @@
+const theActuator = document.querySelector(".theActuator");
+const theMovingThing = document.querySelector(".theMovingThing");
+const theScrim = document.querySelector(".theScrim");
+
+theActuator.addEventListener("click", () => {
+  switch (theMovingThing.style.transform) {
+    case `translateX(100%)`:
+      theMovingThing.style.transform = `translateX(0%)`;
+      theActuator.style.backgroundColor = `#6a4aab`;
+      theScrim.style.visibility = `visible`;
+      break;
+    case `translateX(0%)`:
+      theMovingThing.style.transform = `translateX(100%)`;
+      theActuator.style.backgroundColor = `#ffbf00`;
+      theScrim.style.visibility = `collapse`;
+      break;
+    default:
+      theMovingThing.style.transform = `translateX(0%)`;
+      theActuator.style.backgroundColor = `#6a4aab`;
+      theScrim.style.visibility = `visible`;
+  }
+});
+
+theScrim.addEventListener("click", () => {
+  theMovingThing.style.transform = `translateX(100%)`;
+  theActuator.style.backgroundColor = `#ffbf00`;
+  theScrim.style.visibility = `collapse`;
+});
