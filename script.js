a0be6ebe485476abbb8fb5ce8d243219f479c9e5
Yacyn CHERRADI <cherradiyacyn@gmail.com>
2024-05-03 11:38:52 +0100

    opacity : to get smooth transitions + visibility : to enable/disable events

--- script.js
+++ script.js
@@ -7,16 +7,19 @@ theActuator.addEventListener("click", () => {
     case `translateX(100%)`:
       theMovingThing.style.transform = `translateX(0%)`;
       theActuator.style.backgroundColor = `#6a4aab`;
+      theScrim.style.opacity = `1`;
       theScrim.style.visibility = `visible`;
       break;
     case `translateX(0%)`:
       theMovingThing.style.transform = `translateX(100%)`;
       theActuator.style.backgroundColor = `#ffbf00`;
+      theScrim.style.opacity = `0`;
       theScrim.style.visibility = `collapse`;
       break;
     default:
       theMovingThing.style.transform = `translateX(0%)`;
       theActuator.style.backgroundColor = `#6a4aab`;
+      theScrim.style.opacity = `1`;
       theScrim.style.visibility = `visible`;
   }
 });
@@ -24,5 +27,6 @@ theActuator.addEventListener("click", () => {
 theScrim.addEventListener("click", () => {
   theMovingThing.style.transform = `translateX(100%)`;
   theActuator.style.backgroundColor = `#ffbf00`;
+  theScrim.style.opacity = `0`;
   theScrim.style.visibility = `collapse`;
 });
